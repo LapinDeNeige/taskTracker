d99a29a18caaa3415ae3b382b71cdf982f084c6c
Me <mail@mail.com>
2025-06-04 21:51:18 +0300

    Updated

---
+++ migrations/m241021_112653_tasks_data.php
@@ -0,0 +1,55 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m241021_112653_create_user_data
+ */
+class m241021_112653_create_user_data extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m241021_112653_create_user_data cannot be reverted.\n";
+
+        return false;
+    }
+
+    
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+		$this->createTable('tasks_data',
+		['Task'=>$this->string(50),
+		//'taskNumber'=>$this->integer(5)->notNull(),
+        'taskNumber'=>$this->primaryKey(11),
+		'taskStatus'=>$this->string(20),
+		'Task_start_date'=>$this->date(),
+		'Task_end_date'=>$this->date(),
+		'taskInfo'=>$this->string(90),
+        'Date'=>$this->dateTime();
+		]);
+        
+        ///
+        $this->alterColumn('{{%tasks_data}}','taskNumber',$this->integer(8).' NOT NULL AUTO_INCREMENT');
+		///
+    }
+
+    public function down()
+    {
+        echo "m241021_112653_create_user_data cannot be reverted.\n";
+
+        return false;
+    }
+    
+}
